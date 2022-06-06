b76aa7b1c22a1dbbedc26d9eccead41197a6f916
Mohammad Hashim Jalali <hashim.jalali@alumni.fh-aachen.de>
2022-06-06 21:35:38 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,105 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ReactDOM } from 'react';
 import './App.css';
 
 function App() {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // user login info (react list)
+  const data = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // find user login info
+    const userData = data.find((user) =>
+      user.username === uname.value
+    );
+
+    // compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // invalid password
+        setErrorMessages({ name: "pass", message: errors.pass});
+      } 
+      else {
+        setIsSubmitted(true);
+      }
+    }
+    else {
+      // username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+
+  // JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+
+        <div className="button-container">
+          <input type="submit" value="submit"/>
+        </div>
+      </form>
+
+    </div>
+  );
+
   return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title">
+          Sign In
+        </div>
+        {isSubmitted ? <div>User is succesfully logged in</div> : renderForm}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +117,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+  */
